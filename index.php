da30b3fe956b6aa6e1b7075f4a2310a78cafb67d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-12 23:23:40 +0700

    Update

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $file = exist([
     $folder . '.page'
 ], 1);
 
-if ($file && !is_file($folder . D . 'time.data')) {
+if ($file && !is_file($data = $folder . D . 'time.data')) {
     if (!is_dir($folder)) {
         mkdir($folder, 0775, true);
     }
-    file_put_contents($folder . D . 'time.data', filectime($file));
+    file_put_contents($data, date('Y-m-d H:i:s', filectime($file)));
 }
