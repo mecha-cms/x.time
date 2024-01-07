70b20a6caac1f152becdf94c0901ef51dde98deb
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-01-07 08:48:58 +0700

    Fix Time Zone
    Make sure to run the task after user time zone is set.

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
-$path = trim($url->path ?? $state->route ?? 'index', '/');
-$folder = LOT . D . 'page' . D . $path;
-$file = exist([
-    $folder . '.archive',
-    $folder . '.page'
-], 1);
-
-if ($file && !is_file($data = $folder . D . 'time.data')) {
-    if (!is_dir($folder)) {
-        mkdir($folder, 0775, true);
+Hook::set('route', function ($content, $path) use ($state) {
+    $path = trim($path ?? $state->route ?? 'index', '/');
+    $folder = LOT . D . 'page' . D . $path;
+    $file = exist([
+        $folder . '.archive',
+        $folder . '.page'
+    ], 1);
+    if ($file && !is_file($data = $folder . D . 'time.data')) {
+        if (!is_dir($folder)) {
+            mkdir($folder, 0775, true);
+        }
+        file_put_contents($data, date('Y-m-d H:i:s', filectime($file)));
     }
-    file_put_contents($data, date('Y-m-d H:i:s', filectime($file)));
-}
+}, 0);
